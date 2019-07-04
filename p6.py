b281f8cb0dd17bba962dd9b643ee55b91fa56d10
saikumar246 <52447619+saikumar246@users.noreply.github.com>
2019-07-04 11:47:12 +0530

    Update p6.py

--- p6.py
+++ p6.py
@@ -1,10 +1,10 @@
 s1,s2=map(str,input().split())
 if(len(s1)!=len(s2)):
     print("no")
-a=[s1.count(i) for i in s1]
-b=[s2.count(i) for i in s2]
+x=[s1.count(i) for i in s1]
+y=[s2.count(i) for i in s2]
 
-if(a==b):
+if(x==y):
     print("yes")
 else:
     print("no")
