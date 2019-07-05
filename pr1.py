ce78e2e04abab68f4cb0c0774dce096f82cec301
saikumar246 <52447619+saikumar246@users.noreply.github.com>
2019-07-05 14:57:14 +0530

    Create pr1.py

---
+++ pr1.py
@@ -0,0 +1,13 @@
+word= int(input())
+b=[]
+for i in range(0,word):
+ inpu=input()
+ b.append(inpu)
+li=[]
+for i in zip(*b):
+ if(i.count(i[0])==len(i)):
+  li.append(i[0])
+ 
+ else:
+  break
+print(''.join(li))
